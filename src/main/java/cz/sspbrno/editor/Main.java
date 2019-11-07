bd6a7aad366258a3e57363336e107481717cc963
Pawlost <mr.p.balusek@gmail.com>
2019-11-07 12:56:09 +0100

    Maven for Javafx

---
+++ src/main/java/cz/sspbrno/editor/Main.java
@@ -0,0 +1,9 @@
+package cz.sspbrno.editor;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        App.run(args);
+    }
+}
---
+++ src/main/java/cz/sspbrno/editor/controlers/Editor.java
@@ -0,0 +1,5 @@
+package cz.sspbrno.editor.controlers;
+
+public class Editor 
+{
+}
